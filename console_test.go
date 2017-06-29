f6ecb30e4af096c457da6d54eeb0e639d49f24c3
Daniel Dao <dqminh89@gmail.com>
2017-06-29 23:25:55 +0100

    add a test for console I/O
    Signed-off-by: Daniel Dao <dqminh89@gmail.com>

--- console_test.go
+++ console_test.go
@@ -2,7 +2,15 @@
 
 package console
 
-import "testing"
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+	"sync"
+	"testing"
+)
 
 func TestWinSize(t *testing.T) {
 	c, _, err := NewPty()
@@ -27,5 +35,50 @@ func TestWinSize(t *testing.T) {
 	}
 	if size.Height != 10 {
 		t.Errorf("height should be 10 but received %d", size.Height)
+	}
+}
+
+func TestConsolePty(t *testing.T) {
+	console, slavePath, err := NewPty()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer console.Close()
+
+	slave, err := os.OpenFile(slavePath, os.O_RDWR, 0)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer slave.Close()
+
+	iteration := 10
+
+	cmd := exec.Command("sh", "-c", fmt.Sprintf("for x in `seq 1 %d`; do echo -n test; done", iteration))
+	cmd.Stdin = slave
+	cmd.Stdout = slave
+	cmd.Stderr = slave
+
+	var (
+		b  bytes.Buffer
+		wg sync.WaitGroup
+	)
+	wg.Add(1)
+	go func() {
+		io.Copy(&b, console)
+		wg.Done()
+	}()
+
+	if err := cmd.Run(); err != nil {
+		t.Fatal(err)
+	}
+	slave.Close()
+	wg.Wait()
+
+	expectedOutput := ""
+	for i := 0; i < iteration; i++ {
+		expectedOutput += "test"
+	}
+	if out := b.String(); out != expectedOutput {
+		t.Errorf("unexpected output %q", out)
 	}
 }
